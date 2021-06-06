f04c6bca4506e3c43bb382ba19e6055dfdd0543d
Sashin Amichand <sashin.amichand@icloud.com>
2021-06-07 11:07:16 +1200

    Converted everything to Flask (Python web framework)

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # basketball-project
+If you wish to configure/edit the website, make sure to run the requirments.txt in a 
+virtual env and then install it, else it will install on your pc. 
+Steps to creating virtual env in powershell:
+1. python -m venv basketballproject
+2. basketballproject/Scripts/activate
+3. $env:FLASK_APP="indexFlask"
+4. flask run (to start the website, ctrl + c to stop it and then rerun it in order to view any changes.)
---
+++ indexFlask.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+# Create a Flask Instance
+app = Flask(__name__)
+
+# Create a route decorator
+@app.route('/')
+
+def index():
+    return render_template("index.html")
+
+# Create a route decorator
+@app.route('/junior_breakers')
+
+def junior_breakers():
+    return render_template("junior_breakers.html")
+
+# Create a route decorator
+@app.route('/community')
+
+def community():
+    return render_template("community.html")
+
+# Create a route decorator
+@app.route('/about')
+
+def about():
+    return render_template("about.html")
