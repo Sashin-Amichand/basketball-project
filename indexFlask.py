aaabb267ff845f356c18a485ffee068cf88b59d2
Sashin Amichand <sashin.amichand@icloud.com>
2021-06-07 15:07:52 +1200

    test

--- indexFlask.py
+++ indexFlask.py
@@ -4,27 +4,26 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Create a route decorator
-@app.route('/index')
-@app.route('/')
+@app.route('./')
 
 
 def index():
     return render_template("index.html")
 
 # Create a route decorator
-@app.route('/junior_breakers')
+@app.route('./junior_breakers')
 
 def junior_breakers():
     return render_template("junior_breakers.html")
 
 # Create a route decorator
-@app.route('/community')
+@app.route('./community')
 
 def community():
     return render_template("community.html")
 
 # Create a route decorator
-@app.route('/about')
+@app.route('./about')
 
 def about():
     return render_template("about.html")
